d43aafc73a5574065753fd9ccc16718c684cb74a
Sean <sean@millersoftltd.com>
2020-10-07 13:11:41 +0100

    Added detokenisation method to line chart hover over

--- src/client/components/color-swabs/color-swabs.tsx
+++ src/client/components/color-swabs/color-swabs.tsx
@@ -15,6 +15,7 @@
  */
 
 import * as React from "react";
+import {DetokenisationValue} from "../../components/detokenisation/detokenisation"
 import "./color-swabs.scss";
 
 export interface ColorEntry {
@@ -36,7 +37,7 @@ export const ColorSwabs: React.SFC<ColorSwabsProps> = ({ colorEntries }) => {
       <td>
         <div className="color-swab" style={swabStyle} />
       </td>
-      <td className="color-name">{name}_detokenised</td>
+      <td className="color-name"><DetokenisationValue token={name}></DetokenisationValue></td>
       <td className="color-value">{value}</td>
       {previous && <td className="color-previous">{previous}</td>}
       {delta && <td className="color-delta">{delta}</td>}
